2338031832260fdcff7f10752e02370bbb21d9ac
this-is-manjeet <manjeet41755@gmail.com>
2025-04-23 15:02:03 +0530

    Updated

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
-const closeSidebar = document.getElementById("close-sidebar");
-const openSidebar  = document.getElementById("open-sidebar");
 const navbar = document.querySelector(".nav");
 
-closeSidebar.addEventListener("click", () => {
-    navbar.classList.remove("show");
-});
-
-openSidebar.addEventListener("click", () => {
-    navbar.classList.add("show");
-});
+const toggleSidebar = () => {
+    navbar.classList.toggle("show");
+}
